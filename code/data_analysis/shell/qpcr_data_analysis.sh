7d5f0452947dbb7963dd71eac4963aa46e358548
Brian Williamson <brianw26@uw.edu>
2019-03-11 11:49:53 -0700

    add in shell script for data analysis

--- code/data_analysis/shell/qpcr_data_analysis.sh
+++ code/data_analysis/shell/qpcr_data_analysis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript code/R/analyze_data/qpcr_analysis.R --estimator ${1} --do-parallel ${2} --fold-num ${3} --n-chains ${4} --n-iter ${5} --n-burnin ${6} --q ${7} --num-folds ${8} --sample-num ${9} --q-obs ${10} --leave-one-out ${11} --div-num ${12} --save-stan-model ${13}
+Rscript ../R/qpcr_analysis.R --estimator ${1} --do-parallel ${2} --fold-num ${3} --n-chains ${4} --n-iter ${5} --n-burnin ${6} --q ${7} --num-folds ${8} --sample-num ${9} --q-obs ${10} --leave-one-out ${11} --div-num ${12} --save-stan-model ${13}
